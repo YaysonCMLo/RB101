67757c1721e0b54cb30b4aa83b97d65e315bb727
Jason Lo <jason.chun.man.lo@gmail.com>
2021-05-29 20:15:25 +0000

    calculator version 1.0

--- lesson_2/mortgage_calculator.rb
+++ lesson_2/mortgage_calculator.rb
@@ -4,42 +4,77 @@
 
 # collect inputs
 
+require 'yaml'
+MESSAGES = YAML.load_file('mortgage_messages.yaml')
+
 def prompt(string)
   puts "==> #{string}"
 end
 
-def num_check(message) # This will fail if we receive a float
-  prompt("What is the #{message}")
+def string_check(input)
+  input.empty? == false
+end
+
+def int_check(input)
+  is_int = input.to_i.to_s == input
+  non_zero = input.to_i != 0
+  empty = input.empty? == false
+  is_int && non_zero && empty
+end
+
+def float_check(input)
+  non_zero = input.to_i != 0
+  empty = input.empty? == false
+  is_f = input.to_f.to_s == input
+  is_int = input.to_i.to_s == input
+  (is_f || is_int) && non_zero && empty
+end
+
+def input_check(message, check_type, lead = "What is the")
+  prompt("#{lead} #{message}")
   valid = false
   until valid
-    x = gets.chomp
-    if x.to_i.to_s == x
-      valid = true
-      return x
+    resp = gets.chomp
+    case check_type
+    when 'string' then valid = string_check(resp)
+    when 'int' then valid = int_check(resp)
+    when 'float' then valid = float_check(resp)
     end
-  prompt('Invalid number, please try again')
+    prompt(MESSAGES['invalid']) unless valid
   end
 end
 
-# need to validate these numbers
-loan_amt = num_check("Loan Amount").to_i
-interest_year = num_check('Annual Percentage Rate').to_f/100
-duration_year = num_check('Loan duration (full years)').to_i
+# Welcome message
 
-prompt("Are there any additional months (i.e. partial years). Y if yes")
+user_id = input_check(MESSAGES['name?'], 'string', "")
+prompt("Hello #{user_id}")
+
+# need to validate these numbers
+loan_amt = input_check(MESSAGES['loan_amt'], 'int').to_i
+interest_year = input_check(MESSAGES['int_yr'], 'float').to_f / 100
+duration_year = input_check(MESSAGES['dur_year'], 'int').to_i
+
+prompt(MESSAGES['dur_yr_partial'])
 months_add = gets.chomp
-months_add.downcase == 'y' ? duration_month = num_check('Loan duration (months in partial year)').to_i : duration_month = 0
+duration_month = 0
+if months_add.downcase == 'y'
+  duration_month = input_check(MESSAGES['dur_mnth'], 'string').to_i
+else
+  duration_month = 0
+end
 
 # calculate the inputs to the mortgage rate
 
 interest_month = interest_year / 12
 duration_month += duration_year * 12
-payment_month  = loan_amt * (interest_month / (1 - (1 + interest_month)**(-duration_month)))
+payment_month = loan_amt * (interest_month / (1 - (1 + interest_month)**(-duration_month)))
+tot_cost = payment_month * duration_month
+int_load = tot_cost - loan_amt
 
 invoice = <<-MSG
-==> Your monthly payment is $#{format('%.2f',payment_month.round(2))}
-==> Total cost of the loan is $#{format('%.2f',payment_month * duration_month)}
-==> You are paying $#{format('%.2f',payment_month * duration_month - loan_amt)} in interest!
+#{MESSAGES['mnth_pay']} $#{format('%.2f', payment_month)}
+#{MESSAGES['tot_cost']} $#{format('%.2f', tot_cost)}
+#{MESSAGES['tot_int']} $#{format('%.2f', int_load)} in interest!
 MSG
 
 puts invoice
